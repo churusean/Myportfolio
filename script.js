2789a3e8acd1d5b3de01f042e7863df9efd56888
churusean <churusean@gmail.com>
2022-03-06 19:14:21 +0200

    js fix

--- script.js
+++ script.js
@@ -10,11 +10,8 @@ text.reveal(5000);
 const togglebutton = document.getElementsByClassName('toggle-button')[0]
 const navbarlink = document.getElementsByClassName('bar')[0]
 const menubar = document.getElementsByClassName('menubar')[0]
-const banner = document.getElementsByClassName('banner')[0]
-const myface = document.getElementsByClassName('myface')[0]
+
 togglebutton.addEventListener('click', () => {
     navbarlink.classList.toggle('active')
     menubar.classList.toggle('active')
-    banner.classList.toggle('active')
-    myface.classList.toggle('active')
 })
