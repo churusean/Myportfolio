ab98eade15eef712692f2c0acd121955ca9c038f
churusean <churusean@gmail.com>
2022-03-08 17:14:10 +0200

    scroll

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ togglebutton.addEventListener('click', () => {
     navbarlink.classList.toggle('active')
     menubar.classList.toggle('active')
 })
+
+
+const makeNavLinksSmooth = () => {
+    const navLinks = document.querySelectorAll('.navbar-link');
+
+    for (let n in navLinks) {
+        if (navLinks.hasOwnProperty(n)) {
+            navLinks[n].addEventListener('click', e => {
+                e.preventDefault();
+                document.querySelector(navLinks[n].hash)
+                    .scrollIntoView({
+                        behavior: "smooth"
+                    });
+            });
+        }
+    }
+}
+
+const spyScrolling = () => {
+    const sections = document.querySelectorAll('.section');
+
+    window.onscroll = () => {
+        const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+        for (let s in sections)
+            if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
+                const id = sections[s].id;
+                document.querySelector('.activebar').classList.remove('activebar');
+                document.querySelector(`a[href*=${ id }]`).parentNode.classList.add('activebar');
+            }
+    }
+}
+
+makeNavLinksSmooth();
+spyScrolling();
